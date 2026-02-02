38eecb6030058c32d9f7180aa325efe15ea5b127
seoyeonjiin <duswls214@khu.ac.kr>
2026-02-02 19:30:06 +0900

    feat:queue service 로직 추가

---
+++ src/main/java/com/example/redis_playground/seat/SeatQueueService.java
@@ -0,0 +1,22 @@
+package com.example.redis_playground.seat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SeatQueueService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public Long joinQueue(final Long seatId, final String userId) {
+        final String key = "queue:seat:" + seatId;
+        final double score = System.currentTimeMillis();
+
+        redisTemplate.opsForZSet().add(key, userId, score);
+
+        final Long rank = redisTemplate.opsForZSet().rank(key, userId);
+        return rank == null ? null : rank + 1;
+    }
+}
