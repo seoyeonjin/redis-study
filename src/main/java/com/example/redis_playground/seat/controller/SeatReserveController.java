8772c3733407bddff14f8af572b508a82f99156a
seoyeonjiin <duswls214@khu.ac.kr>
2026-02-04 01:22:41 +0900

    refactor: reserve controller 분리

---
+++ src/main/java/com/example/redis_playground/seat/controller/SeatReserveController.java
@@ -0,0 +1,30 @@
+package com.example.redis_playground.seat.controller;
+
+import com.example.redis_playground.seat.service.SeatReserveService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Profile({"reserve", "local"})
+@RestController
+@RequestMapping("/seats")
+@RequiredArgsConstructor
+public class SeatReserveController {
+
+    public static final String USER_ID_HEADER = "X-USER-ID";
+    private final SeatReserveService seatReserveService;
+
+    @PostMapping("/{seatId}/reserve")
+    public ResponseEntity<Void> reserve(
+            @PathVariable Long seatId,
+            @RequestHeader(USER_ID_HEADER) String userId
+    ) {
+        seatReserveService.reserve(seatId, userId);
+        return ResponseEntity.ok().build();
+    }
+}
