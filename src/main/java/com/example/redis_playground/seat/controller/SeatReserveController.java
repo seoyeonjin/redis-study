8758780d0ed1c92fa36a53ffe7103889ebbcfc22
seoyeonjiin <duswls214@khu.ac.kr>
2026-02-04 17:26:29 +0900

    fix: 필요없는 프로필 설정 제거

--- src/main/java/com/example/redis_playground/seat/controller/SeatReserveController.java
+++ src/main/java/com/example/redis_playground/seat/controller/SeatReserveController.java
@@ -2,7 +2,6 @@ package com.example.redis_playground.seat.controller;
 
 import com.example.redis_playground.seat.service.SeatReserveService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile({"reserve", "local"})
 @RestController
 @RequestMapping("/seats")
 @RequiredArgsConstructor
